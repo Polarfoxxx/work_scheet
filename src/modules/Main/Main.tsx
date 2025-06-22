ba8488c7cf632d9d3659b82c90de2992347a7f20
michalSuchovsky <suchovsky.michal@gmail.com>
2025-06-22 19:53:43 +0200

    Implement form handling and speed calculation in Main component

--- src/modules/Main/Main.tsx
+++ src/modules/Main/Main.tsx
@@ -1,9 +1,43 @@
 import React from "react";
 
 function Main(): React.JSX.Element {
+const [returnedSpeed, setreturnedSpeed] = React.useState<number>(0);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const diametro = formData.get("inputField");
+    const cutSpeed = formData.get("inputFieldSecond");
+
+    if (diametro && cutSpeed) {
+      const speed = calculateSpeed(+diametro, +cutSpeed);
+      setreturnedSpeed(speed);
+    }
+  }
+
+  const calculateSpeed = (diametro: number, cutSpeed: number): number => {
+    const calculateOne = cutSpeed * 60;
+    const calculateTwo = (diametro / 1000) * 3.14;
+    const calculateSpeed = calculateOne / calculateTwo;
+    return calculateSpeed;
+  }
+
+
   return (
     <div className="main">
       <h1>Welcome to the Main Module</h1>
+      <div>
+        <form action="#" onSubmit={e => handleSubmit(e)}>
+          <label htmlFor="inputField">Input Field:</label>
+          <input type="number" id="inputField" name="inputField" />
+          <label htmlFor="inputFieldSecond">Input inputFieldSecond:</label>
+          <input type="number" id="inputFieldSecond" name="inputFieldSecond" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div>
+{returnedSpeed}
+      </div>
       <p>This is the main content area.</p>
     </div>
   );
