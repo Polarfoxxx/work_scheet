1e8a2e0ef9d77a60884797bd93ffe23168aca02e
michalSuchovsky <suchovsky.michal@gmail.com>
2025-08-03 20:05:33 +0200

    Add dependencies for Express, Joi, and Nodemon in package.json

--- client/src/modules/Main/Main.tsx
+++ client/src/modules/Main/Main.tsx
@@ -8,28 +8,12 @@ function Main(): React.JSX.Element {
   return (
     <div className="main">
       <div>
-<<<<<<< HEAD:client/src/modules/Main/Main.tsx
         <nav>
           <NavLink to="/whell-live" className="nav-link">Whell Live</NavLink>
           <NavLink to="/speedCut" className="nav-link">Speed Cut</NavLink>
           {/* Add more navigation links as needed */}
         </nav>
       </div>
-=======
-        <nav className="main-nav">
-          <ul>
-            <li>
-              <NavLink to="/whell-live">Whell Live</NavLink>
-            </li>
-            <li>
-              <NavLink to="/speedCut">Speed Cut</NavLink>
-            </li>
-            {/* Add more navigation links as needed */}
-          </ul>
-        </nav>
-      </div>
-      <div>
->>>>>>> e8c4e04f880bb62def5749772cb44325affaf3a8:src/modules/Main/Main.tsx
         <Routes>
           <Route path="/" element={<WhellLiveModule />} />
           <Route path="/whell-live" element={<WhellLiveModule />} />
@@ -38,7 +22,6 @@ function Main(): React.JSX.Element {
         </Routes>
       </div>
 
-    </div>
   );
 }
 
