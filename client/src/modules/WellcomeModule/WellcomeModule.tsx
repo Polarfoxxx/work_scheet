cd60e1450b47c55bc86300016600e30c8549c73d
michalSuchovsky <suchovsky.michal@gmail.com>
2025-08-21 20:03:32 +0200

    feat(client): add WellcomeModule with logo and style adjustments

--- client/src/modules/WellcomeModule/WellcomeModule.tsx
+++ client/src/modules/WellcomeModule/WellcomeModule.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import "./style/wellcomeModule_style.css";
 
 function WellcomeModule(): React.JSX.Element {
   return (
-    <div>
-      <h1>Welcome to the Wellcome Module</h1>
-      <p>This module is designed to provide an introduction and overview of the application.</p>
+    <div className="wellcome-module">
+      <div className="left-section">
+        <img src="/img/logo.png" alt="" />
+      </div>
+      <div className="right-section">
+        <h1>Welcome to the Wellcome Module</h1>
+        <p>This module is designed to provide an introduction and overview of the application.</p>
+      </div>
     </div>
   );
 }
