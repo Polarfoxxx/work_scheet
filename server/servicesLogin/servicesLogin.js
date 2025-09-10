c09b0728e693cf2eca572763c6454587328d7584
michalSuchovsky <suchovsky.michal@gmail.com>
2025-09-10 19:30:49 +0200

    feat(server): update login API to use POST method and improve error handling feat(client): modify servicesLogin_Api to use POST request for user login chore: clean up environment variable loading in MongoDB connection

--- server/servicesLogin/servicesLogin.js
+++ server/servicesLogin/servicesLogin.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const connectDB = require("../DB/connect_to_mongoo");
 
-router.get("/user", async (req, res) => {
-    const { userName, password } = req.body;
-    console.log("Received login request:", { userName, password });
+router.post("/user", async (req, res) => {
+    const { username, password } = req.body;
+
     try {
+        const data = await connectDB()   // zavoláš funkciu
+        const findUser = data.find(user => user.userName === username && user.password === password);
+        if (!findUser) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
 
-
+        return res.status(200).json({ message: "OK", data });
     } catch (error) {
-        console.error(error);
+        console.error("❌ Chyba v /user route:", error);
         return res.status(500).json({ message: "Internal Server Error" });
-    };
+    }
 });
 
 module.exports = router;
