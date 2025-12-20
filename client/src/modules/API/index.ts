2ce1ab09c2430270396163c9afd6ca42e6cc38de
polar_foxx <suchovsky.michal@gmail.com>
2025-12-20 13:43:10 +0100

    feat: add grindWhellLife_API for fetching wheel life data; update API exports for consistency

--- client/src/modules/API/index.ts
+++ client/src/modules/API/index.ts
@@ -2,6 +2,6 @@ export * from "./servicesLogin";
 export * from "./cutSpeed";
 export * from "./cykleTime";
 export * from "./picAnalyze";
-export * from "./grindWhellLife";
+export * from "./grindingWhellLife";
 export * from "./speed_of_rotation";
 export * from "./signalWhell";
