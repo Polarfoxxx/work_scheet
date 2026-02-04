93a16d4fcadcf89cfb796080e8e9be19836352c2
michalSuchovsky <suchovsky.michal@gmail.com>
2026-02-04 19:27:17 +0100

    feat: implement calculation logic for calculateTz endpoint; enhance response structure with new fields

--- server/calculateFiles/calculateTkz.js
+++ server/calculateFiles/calculateTkz.js
@@ -10,12 +10,26 @@ const {
     pi,
     pow,
     round,
-    sqrt
+    sqrt,
+    boolean
 } = require("mathjs")
 
 router.get("/calculateTz", async (req, res) => {
     const data = {
-        idm: (Number(idm.res.query))
+        idm: (Number(req.query.idm)),
+        adm: (Number(req.query.adm)),
+        breite: (Number(req.query.breite)),
+        aufmass: (Number(req.query.aufmass)),
+        bordmass: (Number(req.query.bordmass)),
+        autotast_zap: req.query.autotast_zap,
+        autotast_dotyk: (Number(req.query.autotast_dotyk)),
+        pridavok: (Number(req.query.pridavok)),
+        v1: (Number(req.query.v1)),
+        v2: (Number(req.query.v2)),
+        v3: (Number(req.query.v3)),
+        sl: (Number(req.query.sl)),
+        t: (Number(req.query.t)),
+        sr: (Number(req.query.sr)),
     };
 
     if (data.idm == undefined) {
@@ -24,7 +38,21 @@ router.get("/calculateTz", async (req, res) => {
         });
     };
 
-    const df = round(e, 3)
+    const obvod_kruhu = 2 * pi * (idm / 2);
+    const plocha = breite * obvod_kruhu;
+    const odobrany_objem = breite * obvod_kruhu * (pridavok / 2);
+
+
+
+    return res.status(200).json({
+        message: {
+            obvod_kruhu: obvod_kruhu,
+            plocha: plocha,
+            prídavok_stenu: pridavok / 2,
+            odobrany_objem: odobrany_objem
+
+        }
+    })
 })
 
 module.exports = router;
