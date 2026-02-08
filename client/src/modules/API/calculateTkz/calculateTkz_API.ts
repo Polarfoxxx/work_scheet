3cbf7bbbfcb8f59a83e5415a58f59c545ae575f8
michalSuchovsky <suchovsky.michal@gmail.com>
2026-02-08 18:52:22 +0100

    feat: update calculateTkz API parameters and endpoint for improved data handling

--- client/src/modules/API/calculateTkz/calculateTkz_API.ts
+++ client/src/modules/API/calculateTkz/calculateTkz_API.ts
@@ -1,39 +1,57 @@
 import axios from "axios";
 
 
-type Type_for_cutSpeed_API_params = {
-    speedOfRotation: number;
-    diametro: number;
+type Type_for_calculateTz_params = {
+    idm: number;
+    adm: number;
+    breite: number,
+    aufmass: number,
+    bordmass: number,
+    autotast_zap: number,
+    autotast_dotyk: number,
+    pridavok: number,
+    rychloposuv: number,
+    zbk1: number,
+    zbk2: number,
+    zbk3: number,
+    zbk4: number,
+    luft: number,
+    v1: number,
+    v2: number,
+    v3: number,
+    sl: number,
+    t: number,
+    sr: number,
 }
 
-async function calculateTkz_API(params: Type_for_cutSpeed_API_params): Promise<{ message: number } | undefined> {
+async function calculateTkz_API(params: Type_for_calculateTz_params): Promise<{ message: number } | undefined> {
 
     try {
-        const response = await axios.get("/calculateTkz_API/cutSpeed", {
+        const response = await axios.get("/calculateCalcTkz/calculateTz", {
             params: {
-                 idm: idm,
-        adm: (Number(req.query.adm)),
-        breite: (Number(req.query.breite)),
-        aufmass: (Number(req.query.aufmass)), /* 0,500 */
-        bordmass: (Number(req.query.bordmass)), /* 0,500 */
-        autotast_zap: req.query.autotast_zap,
-        autotast_dotyk: (Number(req.query.autotast_dotyk)),
-        pridavok: (Number(req.query.pridavok)), /* 0,300 */
-        rychloposuv: (Number(req.query.rychloposuv)),
-        zbk1: (Number(req.query.zbk1)),
-        zbk2: (Number(req.query.zbk2)),
-        zbk3: (Number(req.query.zbk3)),
-        zbk4: (Number(req.query.zbk4)),
-        luft: (Number(req.query.luft)),
-        v1: (Number(req.query.v1)),
-        v2: (Number(req.query.v2)),
-        v3: (Number(req.query.v3)),
-        sl: (Number(req.query.sl)),
-        t: (Number(req.query.t)),
-        sr: (Number(req.query.sr)),
+                idm: params.idm,
+                adm: params.adm,
+                breite: params.breite,
+                aufmass: params.aufmass,
+                bordmass: params.bordmass,
+                autotast_zap: params.autotast_zap,
+                autotast_dotyk: params.autotast_dotyk,
+                pridavok: params.pridavok,
+                rychloposuv: params.rychloposuv,
+                zbk1: params.zbk1,
+                zbk2: params.zbk2,
+                zbk3: params.zbk3,
+                zbk4: params.zbk4,
+                luft: params.luft,
+                v1: params.v1,
+                v2: params.v2,
+                v3: params.v3,
+                sl: params.sl,
+                t: params.t,
+                sr: params.sr,
             }
-        }
-        );
+        });
+
         return response.data;
     } catch (error) {
         console.error(error);
